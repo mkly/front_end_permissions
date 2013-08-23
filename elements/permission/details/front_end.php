a272ebb1873a64618663953d80ac2014d1e0af68
Mike Lay <mike@mkly.io>
2013-08-22 19:34:42 -0700

    Fix loader element permission detail
    Fix loader element permission detail

--- elements/permission/details/front_end.php
+++ elements/permission/details/front_end.php
@@ -1,7 +1,7 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.") ?>
 
 <?php $pk = PermissionKey::getByID($_REQUEST['pkID']) ?>
-<?php Loader::element('permission/detail', array('permissionKey' => $pk), 'front_end_permissions') ?>
+<?php Loader::element('permission/detail', array('permissionKey' => $pk)) ?>
 <script type="text/javascript">
 	var ccm_permissionDialogURL = '<?= REL_DIR_FILES_TOOLS_REQUIRED ?>/permissions/dialogs/front_end';
 </script>
